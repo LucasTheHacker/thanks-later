4e376d26125f81ca67cd4344b597499c60b9cd97
Lucas Batista <106922188+LucasTheHacker@users.noreply.github.com>
2024-05-20 17:22:10 -0300

    Add Department.java and Program.java || tested.

---
+++ jdbc/src/main/java/lucas/application/Program.java
@@ -0,0 +1,15 @@
+package lucas.application;
+
+import lucas.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Department department = new Department(1, "Livros");
+
+        System.out.println(department);
+
+    }
+
+}
